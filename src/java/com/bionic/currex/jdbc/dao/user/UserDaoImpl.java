bf28ed362efc6d2c41c635c6a2297209a90486d6
sergio300 <sergii.zagoruiko@gmail.com>
2013-10-08 01:06:40 +0300

    DAOInProgress3

--- src/java/com/bionic/currex/jdbc/dao/user/UserDaoImpl.java
+++ src/java/com/bionic/currex/jdbc/dao/user/UserDaoImpl.java
@@ -5,6 +5,10 @@
 package com.bionic.currex.jdbc.dao.user;
 
 import com.bionic.currex.entities.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -15,8 +19,40 @@ public class UserDaoImpl implements IUserDao{
 
     @Override
     public int addUser(String firstName, String lastName, String nickName, 
-    String email, String password, int phone, int cell, int userRoleId) throws Exception {
-        
+        String email, String password, int phone, int cell, int userRoleId) throws Exception {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int id = -1;
+        try{
+            ps = conn.prepareStatement("INSERT INTO User(firstName, lastName, "
+                    + "nickName, email, password, phone, cell, userRoleId) "
+                    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?);", 
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, firstName);
+            ps.setString(2, lastName);
+            ps.setString(3, nickName);
+            ps.setString(4, email);
+            ps.setString(5, password);
+            ps.setInt(6, phone);
+            ps.setInt(7, cell);
+            ps.setInt(8, userRoleId);
+            ps.execute();
+            rs = ps.getGeneratedKeys();
+            rs.next();
+            id = rs.getInt(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return id;
+        }
     }
 
     @Override
